4449edb5f93889c4f25f81966962fae93c04b75b
iamjuandev <juanmiguelar09@gmail.com>
2026-02-05 14:05:33 -0600

    impr

--- .nuxt/imports.d.ts
+++ .nuxt/imports.d.ts
@@ -36,4 +36,3 @@ export { useNitroOrigin } from '../node_modules/nuxt-site-config/dist/runtime/nu
 export { useSiteConfig } from '../node_modules/nuxt-site-config/dist/runtime/nuxt/composables/useSiteConfig';
 export { createSitePathResolver, withSiteTrailingSlash, withSiteUrl } from '../node_modules/nuxt-site-config/dist/runtime/nuxt/composables/utils';
 export { useImage } from '../node_modules/@nuxt/image/dist/runtime/composables';
-export { useNuxtDevTools } from '../node_modules/@nuxt/devtools/dist/runtime/use-nuxt-devtools';
